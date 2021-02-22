f1060434f8a7e6a5f70ca6becd87b00bf098aa4b
Joe Hammer <parrhesiajoe@gmail.com>
2021-02-22 14:20:52 -0800

    Initial commit

--- WireGuardTest/Program.cs
+++ WireGuardTest/Program.cs
@@ -6,7 +6,7 @@ namespace WireGuardTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
